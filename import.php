ad8b230e5854369035e0af74bcdb5e259a4dcfc2
ScottNovakowski <snovakow@gmail.com>
2024-08-26 13:06:40 -0400

    Update import.php

--- import.php
+++ import.php
@@ -143,4 +143,9 @@ INSERT INTO `naked3` (`puzzle_id`, `count`)
 SELECT `id`, `naked3`
 FROM `puzzles` AS p
 WHERE p.`naked2`=0 AND p.`naked3`>0 AND p.`naked4`=0 AND p.`hidden2`=0 AND p.`hidden3`=0 AND p.`hidden4`=0 AND p.`yWing`=0 AND p.`xyzWing`=0 AND p.`xWing`=0 AND p.`swordfish`=0 AND p.`jellyfish`=0 AND p.`uniqueRectangle`=0 AND p.`bruteForce`=0
+
+INSERT INTO `phistomefelRing` (`puzzle_id`, `count`)
+SELECT `id`, `phistomefel`
+FROM `phistomefel` AS p
+WHERE p.`naked2`=0 AND p.`naked3`=0 AND p.`naked4`=0 AND p.`hidden2`=0 AND p.`hidden3`=0 AND p.`hidden4`=0 AND p.`yWing`=0 AND p.`xyzWing`=0 AND p.`xWing`=0 AND p.`swordfish`=0 AND p.`jellyfish`=0 AND p.`uniqueRectangle`=0 AND p.`phistomefel`>0 AND p.`bruteForce`=0
 */
