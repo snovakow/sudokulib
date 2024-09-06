f53290f767f6f7f5d97e91291a51a327fb9812f3
ScottNovakowski <snovakow@gmail.com>
2024-09-06 09:36:28 -0400

    Update import.php

--- import.php
+++ import.php
@@ -130,25 +130,3 @@ try {
 }
 
 $pdo = null;
-
-/*
-TRUNCATE TABLE simple
-
-simple naked2 naked3 naked4 hidden2 hidden3 hidden4 omissions yWing xyzWing xWing swordfish jellyfish uniqueRectangle
-has_naked2 has_naked3 has_naked4 has_hidden2 has_hidden3 has_hidden4 has_omissions has_yWing has_xyzWing has_xWing has_swordfish has_jellyfish has_uniqueRectangle
-
-INSERT INTO `simple` (`puzzle_id`)
-SELECT `id`
-FROM `puzzles` AS p
-WHERE p.`simple` > 0
-
-INSERT INTO `naked2` (`puzzle_id`, `count`)
-SELECT `id`, `naked2`
-FROM `puzzles` AS p
-WHERE p.`naked2`>0 AND p.`naked3`=0 AND p.`naked4`=0 AND p.`hidden2`=0 AND p.`hidden3`=0 AND p.`hidden4`=0 AND p.`omissions`=0 AND p.`yWing`=0 AND p.`xyzWing`=0 AND p.`xWing`=0 AND p.`swordfish`=0 AND p.`jellyfish`=0 AND p.`uniqueRectangle`=0 AND p.`bruteForce`=0
-
-INSERT INTO `phistomefelRing` (`puzzle_id`, `count`)
-SELECT `id`, `phistomefel`
-FROM `phistomefel` AS p
-WHERE p.`naked2`=0 AND p.`naked3`=0 AND p.`naked4`=0 AND p.`hidden2`=0 AND p.`hidden3`=0 AND p.`hidden4`=0 AND p.`omissions`=0 AND p.`yWing`=0 AND p.`xyzWing`=0 AND p.`xWing`=0 AND p.`swordfish`=0 AND p.`jellyfish`=0 AND p.`uniqueRectangle`=0 AND p.`phistomefel`>0 AND p.`bruteForce`=0
-*/
