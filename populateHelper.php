a0034e210885d25d6be45a5d2f6c37b1cf233fc4
ScottNovakowski <snovakow@gmail.com>
2024-09-28 10:47:51 -0400

    Update populateHelper.php

--- populateHelper.php
+++ populateHelper.php
@@ -1,5 +1,6 @@
 <?php
-if (!isset($_GET['table'])) die;
+if (!isset($_GET['table'])) die();
+$table = $_GET['table'];
 
 $servername = "localhost";
 $username = "snovakow";
@@ -15,25 +16,23 @@ function flushOut($message)
 	flush();
 }
 
-$single = $_GET['strategy'];
+header('Content-type: text/html; charset=utf-8');
 
 try {
 	$pdo = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-	// $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 	$strategies = array("naked2", "naked3", "naked4", "hidden2", "hidden3", "hidden4", "omissions", "yWing", "xyzWing", "xWing", "swordfish", "jellyfish", "uniqueRectangle");
 
-	$table = $_GET['table'];
-
-	if (!$single || $single == "simple") {
+	if ($table === 'truncate') {
 		$sql = "TRUNCATE TABLE `simple`";
 		flushOut($sql);
 		$statement = $pdo->prepare($sql);
 		if ($execute) $statement->execute();
-
+	} else {
 		$sql = "
-			INSERT INTO `simple` (`puzzle_id`)
-			SELECT `id`
+			INSERT INTO `simple` (`puzzle_id`, `table`)
+			SELECT `id`, '" . $table . "'
 			FROM `" . $table . "` AS p
 			WHERE p.`simple` > 0
 		";
@@ -43,29 +42,29 @@ try {
 	}
 
 	foreach ($strategies as $strategy) {
-		if ($single && $single != $strategy) continue;
+		if ($table === 'truncate') {
+			$sql = "TRUNCATE TABLE `" . $strategy . "`";
+			flushOut($sql);
+			$statement = $pdo->prepare($sql);
+			if ($execute) $statement->execute();
+		} else {
+			$sql = "
+				INSERT INTO `" . $strategy . "` (`puzzle_id`, `count`, `table`)
+				SELECT `id`, `has_" . $strategy . "`, '" . $table . "'
+				FROM `" . $table . "` AS p
+				WHERE  p.`bruteForce`=0  AND p.`has_" . $strategy . "` >0
+			";
+			foreach ($strategies as $name) {
+				// if ($strategy == "jellyfish" && $name == "naked2") continue;
 
-		$sql = "TRUNCATE TABLE `" . $strategy . "`";
-		flushOut($sql);
-		$statement = $pdo->prepare($sql);
-		if ($execute) $statement->execute();
-
-		$sql = "
-			INSERT INTO `" . $strategy . "` (`puzzle_id`, `count`)
-			SELECT `id`, `" . $strategy . "`
-			FROM `" . $table . "` AS p
-			WHERE  p.`bruteForce`=0
-		";
-		foreach ($strategies as $name) {
-			// if ($strategy == "jellyfish" && $name == "naked2") continue;
-
-			$sql .= " AND p.`" . $name . "`";
-			if ($name == $strategy) $sql .= ">0";
-			else $sql .= "=0";
+				$sql .= " AND p.`has_" . $name . "`";
+				if ($name == $strategy) $sql .= ">0";
+				else $sql .= "=0";
+			}
+			flushOut($sql . "<br/>");
+			$statement = $pdo->prepare($sql);
+			if ($execute) $statement->execute();
 		}
-		flushOut($sql . "<br/>");
-		$statement = $pdo->prepare($sql);
-		if ($execute) $statement->execute();
 	}
 } catch (PDOException $e) {
 	echo "Connection failed: " . $e->getMessage();
