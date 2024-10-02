5bcbbbb17d0af522e7300a71b1259e237471a37a
ScottNovakowski <snovakow@gmail.com>
2024-10-02 15:50:58 -0400

    Update feed.php

--- feed.php
+++ feed.php
@@ -30,15 +30,20 @@ function queryStrategy($conn, $table)
 }
 
 // header("Access-Control-Allow-Origin: *");
+
 if (!isset($_GET['mode'])) die;
 
-// -1 = All
 // 0 = Count
-// 1 = Clues
-// 2 = Strategies
-// 3 = Stats
+// 1 = Strategies
+// 2 = Clues
+$mode = 0;
+
 $mode = (int)$_GET['mode'];
-if ($mode < -1 || $mode > 5) die;
+if ($mode !== 0 && $mode !== 1 && $mode !== 2) die;
+
+if (!isset($_GET['table'])) {
+	if ($mode === 0 || $mode === 2) die;
+}
 
 $servername = "localhost";
 $username = "snovakow";
@@ -49,79 +54,79 @@ try {
 	$conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
 	// $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION, PDO::ATTR_STRINGIFY_FETCHES);
 
-	if (isset($_GET['table'])) $tables = array($_GET['table']);
-	else $tables = array("puzzles1", "puzzles2", "puzzles3");
+	if ($mode === 1) {
+		flushOut("--- Strategies");
+
+		$naked2 = queryStrategy($conn, 'naked2');
+		$naked3 = queryStrategy($conn, 'naked3');
+		$naked4 = queryStrategy($conn, 'naked4');
+		$hidden2 = queryStrategy($conn, 'hidden2');
+		$hidden3 = queryStrategy($conn, 'hidden3');
+		$hidden4 = queryStrategy($conn, 'hidden4');
+		$omissions = queryStrategy($conn, 'omissions');
+		$yWing = queryStrategy($conn, 'yWing');
+		$xyzWing = queryStrategy($conn, 'xyzWing');
+		$xWing = queryStrategy($conn, 'xWing');
+		$swordfish = queryStrategy($conn, 'swordfish');
+		$jellyfish = queryStrategy($conn, 'jellyfish');
+		$uniqueRectangle = queryStrategy($conn, 'uniqueRectangle');
+
+		$candidates = 0;
+		$candidates += $naked2['count'];
+		$candidates += $naked3['count'];
+		$candidates += $naked4['count'];
+		$candidates += $hidden2['count'];
+		$candidates += $hidden3['count'];
+		$candidates += $hidden4['count'];
+		$candidates += $omissions['count'];
+		$candidates += $yWing['count'];
+		$candidates += $xyzWing['count'];
+		$candidates += $xWing['count'];
+		$candidates += $swordfish['count'];
+		$candidates += $jellyfish['count'];
+		$candidates += $uniqueRectangle['count'];
+
+		if ($candidates > 0) {
+			printStat("Naked 2 (" . $naked2['max'] . ")", $naked2['count'], $candidates);
+			printStat("Naked 3 (" . $naked3['max'] . ")", $naked3['count'], $candidates);
+			printStat("Naked 4 (" . $naked4['max'] . ")", $naked4['count'], $candidates);
+			printStat("Hidden 2 (" . $hidden2['max'] . ")", $hidden2['count'], $candidates);
+			printStat("Hidden 3 (" . $hidden3['max'] . ")", $hidden3['count'], $candidates);
+			printStat("Hidden 4 (" . $hidden4['max'] . ")", $hidden4['count'], $candidates);
+			printStat("Omissions (" . $omissions['max'] . ")", $omissions['count'], $candidates);
+			printStat("yWing (" . $yWing['max'] . ")", $yWing['count'], $candidates);
+			printStat("xyzWing (" . $xyzWing['max'] . ")", $xyzWing['count'], $candidates);
+			printStat("xWing (" . $xWing['max'] . ")", $xWing['count'], $candidates);
+			printStat("swordfish (" . $swordfish['max'] . ")", $swordfish['count'], $candidates);
+			printStat("jellyfish (" . $jellyfish['max'] . ")", $jellyfish['count'], $candidates);
+			printStat("uniqueRectangle (" . $uniqueRectangle['max'] . ")", $uniqueRectangle['count'], $candidates);
+		}
+		echo  "<br/>";
+		die;
+	}
+
+	$tables = explode(",", $_GET['table']);
 
 	$total = 0;
+	$totals = array();
 	foreach ($tables as $table) {
 		$stmt = $conn->prepare("
-			SELECT MAX(id) as totalPuzzles FROM `" . $table . "`
-		");
+				SELECT MAX(id) as totalPuzzles FROM `" . $table . "`
+			");
 		$stmt->execute();
 		$totalPuzzles = $stmt->fetch()["totalPuzzles"];
 		$total += $totalPuzzles;
-		flushOut($table . ": " . number_format($totalPuzzles));
+		$totals[] =  $table . "=" . $totalPuzzles;
+		if ($mode !== 0) flushOut($table . ": " . number_format($totalPuzzles));
 	}
-	if (count($tables) > 1) flushOut("Total Puzzles: " . number_format($total));
-	echo  "<br/>";
-
-	if ($mode === 1 || $mode === -1) {
-		if ($table == "phistomefel") {
-			$phistomefel = queryStrategy($conn, 'phistomefelRing');
-			printStat("Phistomefel Isolated", $phistomefel['count'], $total);
-			echo  "<br/>";
-		} else {
-			flushOut("--- Strategies");
-
-			$naked2 = queryStrategy($conn, 'naked2');
-			$naked3 = queryStrategy($conn, 'naked3');
-			$naked4 = queryStrategy($conn, 'naked4');
-			$hidden2 = queryStrategy($conn, 'hidden2');
-			$hidden3 = queryStrategy($conn, 'hidden3');
-			$hidden4 = queryStrategy($conn, 'hidden4');
-			$omissions = queryStrategy($conn, 'omissions');
-			$yWing = queryStrategy($conn, 'yWing');
-			$xyzWing = queryStrategy($conn, 'xyzWing');
-			$xWing = queryStrategy($conn, 'xWing');
-			$swordfish = queryStrategy($conn, 'swordfish');
-			$jellyfish = queryStrategy($conn, 'jellyfish');
-			$uniqueRectangle = queryStrategy($conn, 'uniqueRectangle');
-
-			$candidates = 0;
-			$candidates += $naked2['count'];
-			$candidates += $naked3['count'];
-			$candidates += $naked4['count'];
-			$candidates += $hidden2['count'];
-			$candidates += $hidden3['count'];
-			$candidates += $hidden4['count'];
-			$candidates += $omissions['count'];
-			$candidates += $yWing['count'];
-			$candidates += $xyzWing['count'];
-			$candidates += $xWing['count'];
-			$candidates += $swordfish['count'];
-			$candidates += $jellyfish['count'];
-			$candidates += $uniqueRectangle['count'];
-
-			if ($candidates > 0) {
-				printStat("Naked 2 (" . $naked2['max'] . ")", $naked2['count'], $candidates);
-				printStat("Naked 3 (" . $naked3['max'] . ")", $naked3['count'], $candidates);
-				printStat("Naked 4 (" . $naked4['max'] . ")", $naked4['count'], $candidates);
-				printStat("Hidden 2 (" . $hidden2['max'] . ")", $hidden2['count'], $candidates);
-				printStat("Hidden 3 (" . $hidden3['max'] . ")", $hidden3['count'], $candidates);
-				printStat("Hidden 4 (" . $hidden4['max'] . ")", $hidden4['count'], $candidates);
-				printStat("Omissions (" . $omissions['max'] . ")", $omissions['count'], $candidates);
-				printStat("yWing (" . $yWing['max'] . ")", $yWing['count'], $candidates);
-				printStat("xyzWing (" . $xyzWing['max'] . ")", $xyzWing['count'], $candidates);
-				printStat("xWing (" . $xWing['max'] . ")", $xWing['count'], $candidates);
-				printStat("swordfish (" . $swordfish['max'] . ")", $swordfish['count'], $candidates);
-				printStat("jellyfish (" . $jellyfish['max'] . ")", $jellyfish['count'], $candidates);
-				printStat("uniqueRectangle (" . $uniqueRectangle['max'] . ")", $uniqueRectangle['count'], $candidates);
-			}
-			echo  "<br/>";
-		}
+	if ($mode === 0) {
+		echo implode(",", $totals);
+	} else {
+		if (count($tables) > 1) flushOut("Total Puzzles: " . number_format($total));
+		echo  "<br/>";
 	}
 
-	if ($mode === 2 || $mode === -1) {
+	if ($mode === 2) {
 		flushOut("--- Clues");
 		$counts = array();
 		$count0 = array();
