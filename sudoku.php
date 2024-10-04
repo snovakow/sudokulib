5c08d92a6b2ed33f3b75b27b69be59e3838f21a1
ScottNovakowski <snovakow@gmail.com>
2024-10-04 17:17:54 -0400

    Update sudoku.php

--- sudoku.php
+++ sudoku.php
@@ -2,13 +2,13 @@
 
 // header("Access-Control-Allow-Origin: *");
 
-if (!isset($_GET['uid'])) die;
 if (!isset($_GET['strategy'])) die;
 $type = $_GET['strategy'];
 
 $strategy = false;
 
 if ($type == 'simple') $strategy = $type;
+if ($type == 'bruteForce') $strategy = $type;
 if ($type == 'naked2') $strategy = $type;
 if ($type == 'naked3') $strategy = $type;
 if ($type == 'naked4') $strategy = $type;
@@ -16,12 +16,13 @@ if ($type == 'hidden2') $strategy = $type;
 if ($type == 'hidden3') $strategy = $type;
 if ($type == 'hidden4') $strategy = $type;
 if ($type == 'omissions') $strategy = $type;
+if ($type == 'uniqueRectangle') $strategy = $type;
 if ($type == 'yWing') $strategy = $type;
 if ($type == 'xyzWing') $strategy = $type;
 if ($type == 'xWing') $strategy = $type;
 if ($type == 'swordfish') $strategy = $type;
 if ($type == 'jellyfish') $strategy = $type;
-if ($type == 'uniqueRectangle') $strategy = $type;
+if ($type == 'custom') $strategy = $type;
 
 if (!$strategy) die();
 
@@ -33,6 +34,21 @@ $dbname = "sudoku";
 try {
 	$conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
 	// $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION, PDO::ATTR_STRINGIFY_FETCHES);
+
+	if ($type == 'custom') {
+		$stmt = $conn->prepare("SELECT `id`, `title`, `puzzle` FROM `hardcoded`");
+		$stmt->execute();
+		$result = $stmt->fetchAll(\PDO::FETCH_ASSOC);
+		$results = array();
+		foreach ($result as $key => $row) {
+			$id = $row['id'];
+			$title = $row['title'];
+			$puzzle = $row['puzzle'];
+			$results[] = $id . "," . $title . "," . $puzzle;
+		}
+		echo implode(":", $results);
+		die;
+	}
 
 	$stmt = $conn->prepare("
 		SELECT s.`puzzle_id`, s.`table` FROM `" . $strategy . "` AS s   
@@ -49,13 +65,11 @@ try {
 	);
 
 	$stmt->execute();
-	$result = $stmt->fetchAll(\PDO::FETCH_ASSOC);
-	foreach ($result as $key => $row) {
-		$id = $row['id'];
-		$puzzleClues = $row['puzzleClues'];
-		$puzzleFilled = $row['puzzleFilled'];
-		echo $id . ":" . $puzzleClues . ":" . $puzzleFilled;
-	}
+	$result = $stmt->fetch();
+	$id = $result['id'];
+	$puzzleClues = $result['puzzleClues'];
+	$puzzleFilled = $result['puzzleFilled'];
+	echo $id . ":" . $puzzleClues . ":" . $puzzleFilled;
 } catch (PDOException $e) {
 	echo "Error: " . $e->getMessage();
 }
