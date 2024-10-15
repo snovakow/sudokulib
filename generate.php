f744058145e883273a6f77a61292625251aafe37
ScottNovakowski <snovakow@gmail.com>
2024-10-15 09:46:23 -0400

    From dev

---
+++ generate.php
@@ -0,0 +1,164 @@
+<?php
+if (!isset($_GET['version'])) die();
+$version = 1;
+if ($_GET['version'] != $version) die();
+
+if (!isset($_GET['puzzleClues'])) die();
+if (!isset($_GET['puzzleFilled'])) die();
+if (!isset($_GET['clueCount'])) die();
+if (!isset($_GET['simple'])) die();
+if (!isset($_GET['naked2'])) die();
+if (!isset($_GET['naked3'])) die();
+if (!isset($_GET['naked4'])) die();
+if (!isset($_GET['hidden2'])) die();
+if (!isset($_GET['hidden3'])) die();
+if (!isset($_GET['hidden4'])) die();
+if (!isset($_GET['omissions'])) die();
+if (!isset($_GET['yWing'])) die();
+if (!isset($_GET['xyzWing'])) die();
+if (!isset($_GET['xWing'])) die();
+if (!isset($_GET['swordfish'])) die();
+if (!isset($_GET['jellyfish'])) die();
+if (!isset($_GET['uniqueRectangle'])) die();
+if (!isset($_GET['phistomefel'])) die();
+if (!isset($_GET['superpositions'])) die();
+if (!isset($_GET['bruteForce'])) die();
+
+if (!isset($_GET['has_naked2'])) die();
+if (!isset($_GET['has_naked3'])) die();
+if (!isset($_GET['has_naked4'])) die();
+if (!isset($_GET['has_hidden2'])) die();
+if (!isset($_GET['has_hidden3'])) die();
+if (!isset($_GET['has_hidden4'])) die();
+if (!isset($_GET['has_omissions'])) die();
+if (!isset($_GET['has_uniqueRectangle'])) die();
+if (!isset($_GET['has_yWing'])) die();
+if (!isset($_GET['has_xyzWing'])) die();
+if (!isset($_GET['has_xWing'])) die();
+if (!isset($_GET['has_swordfish'])) die();
+if (!isset($_GET['has_jellyfish'])) die();
+
+if (!isset($_GET['table'])) die();
+$table = $_GET['table'];
+
+$puzzleClues = $_GET['puzzleClues'];
+$puzzleFilled = $_GET['puzzleFilled'];
+$clueCount = $_GET['clueCount'];
+$simple = $_GET['simple'];
+$naked2 = $_GET['naked2'];
+$naked3 = $_GET['naked3'];
+$naked4 = $_GET['naked4'];
+$hidden2 = $_GET['hidden2'];
+$hidden3 = $_GET['hidden3'];
+$hidden4 = $_GET['hidden4'];
+$omissions = $_GET['omissions'];
+$yWing = $_GET['yWing'];
+$xyzWing = $_GET['xyzWing'];
+$xWing = $_GET['xWing'];
+$swordfish = $_GET['swordfish'];
+$jellyfish = $_GET['jellyfish'];
+$uniqueRectangle = $_GET['uniqueRectangle'];
+$phistomefel = $_GET['phistomefel'];
+$superpositions = $_GET['superpositions'];
+$bruteForce = $_GET['bruteForce'];
+
+$servername = "localhost";
+$username = "snovakow";
+$password = "kewbac-recge1-Fiwpux";
+$dbname = "sudoku";
+
+$solveType = 1;
+if ($simple > 0) $solveType = 0;
+if ($bruteForce > 0) $solveType = 2;
+
+$has_naked2 = $_GET['has_naked2'];
+$has_naked3 = $_GET['has_naked3'];
+$has_naked4 = $_GET['has_naked4'];
+$has_hidden2 = $_GET['has_hidden2'];
+$has_hidden3 = $_GET['has_hidden3'];
+$has_hidden4 = $_GET['has_hidden4'];
+$has_omissions = $_GET['has_omissions'];
+$has_uniqueRectangle = $_GET['has_uniqueRectangle'];
+$has_yWing = $_GET['has_yWing'];
+$has_xyzWing = $_GET['has_xyzWing'];
+$has_xWing = $_GET['has_xWing'];
+$has_swordfish = $_GET['has_swordfish'];
+$has_jellyfish = $_GET['has_jellyfish'];
+
+$has_phistomefel = $phistomefel;
+
+try {
+	$pdo = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+	// $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+	$sql = "SELECT MAX(id) as totalPuzzles FROM `" . $table . "`";
+	$statement = $pdo->prepare($sql);
+	$statement->execute();
+	$result = $statement->fetch();
+	$count = $result['totalPuzzles'];
+
+	if ($count === NULL) {
+		$statement = $pdo->prepare("
+			SELECT table_name FROM information_schema.tables WHERE table_schema = 'sudoku' AND table_name = '" . $table . "' LIMIT 1;
+		");
+		$statement->execute();
+		if ($statement->fetch()["table_name"] === NULL) exit("-1");
+		$count = 0;
+	}
+
+	if ($count >= 10000000) {
+		exit("0");
+	}
+
+	$sql = "INSERT INTO `" . $table . "` (puzzleClues, puzzleFilled, clueCount, simple, naked2, naked3, naked4, hidden2, hidden3, hidden4, omissions, 
+			yWing, xyzWing, xWing, swordfish, jellyfish, uniqueRectangle, phistomefel, has_naked2, has_naked3, has_naked4, has_hidden2, has_hidden3, has_hidden4, has_omissions, 
+			has_yWing, has_xyzWing, has_xWing, has_swordfish, has_jellyfish, has_uniqueRectangle, has_phistomefel, superpositions, bruteForce, solveType) 
+			VALUES (:puzzleClues, :puzzleFilled, :clueCount, :simple, :naked2, :naked3, :naked4, :hidden2, :hidden3, :hidden4, :omissions, 
+			:yWing, :xyzWing, :xWing, :swordfish, :jellyfish, :uniqueRectangle, :phistomefel, :has_naked2, :has_naked3, :has_naked4, :has_hidden2, :has_hidden3, :has_hidden4, :has_omissions, 
+			:has_yWing, :has_xyzWing, :has_xWing, :has_swordfish, :has_jellyfish, :has_uniqueRectangle, :has_phistomefel, :superpositions, :bruteForce, :solveType)";
+
+	$statement = $pdo->prepare($sql);
+
+	$statement->execute([
+		'puzzleClues' => $puzzleClues,
+		'puzzleFilled' => $puzzleFilled,
+		'clueCount' => $clueCount,
+		'simple' => $simple,
+		'naked2' => $naked2,
+		'naked3' => $naked3,
+		'naked4' => $naked4,
+		'hidden2' => $hidden2,
+		'hidden3' => $hidden3,
+		'hidden4' => $hidden4,
+		'omissions' => $omissions,
+		'yWing' => $yWing,
+		'xyzWing' => $xyzWing,
+		'xWing' => $xWing,
+		'swordfish' => $swordfish,
+		'jellyfish' => $jellyfish,
+		'uniqueRectangle' => $uniqueRectangle,
+		'phistomefel' => $phistomefel,
+		'has_naked2' => $has_naked2,
+		'has_naked3' => $has_naked3,
+		'has_naked4' => $has_naked4,
+		'has_hidden2' => $has_hidden2,
+		'has_hidden3' => $has_hidden3,
+		'has_hidden4' => $has_hidden4,
+		'has_omissions' => $has_omissions,
+		'has_yWing' => $has_yWing,
+		'has_xyzWing' => $has_xyzWing,
+		'has_xWing' => $has_xWing,
+		'has_swordfish' => $has_swordfish,
+		'has_jellyfish' => $has_jellyfish,
+		'has_uniqueRectangle' => $has_uniqueRectangle,
+		'has_phistomefel' => $has_phistomefel,
+		'superpositions' => $superpositions,
+		'bruteForce' => $bruteForce,
+		'solveType' => $solveType
+	]);
+	echo ($count + 1);
+} catch (PDOException $e) {
+	// echo "Connection failed: " . $e->getMessage();
+}
+
+$pdo = null;
---
+++ worker.js
@@ -0,0 +1,349 @@
+import { CellCandidate, Grid } from "../sudokulib/Grid.js";
+import { sudokuGenerator, fillSolve, totalPuzzles, STRATEGY, STRATEGIES } from "../sudokulib/generator.js";
+
+const cells = new Grid();
+for (let i = 0; i < 81; i++) cells[i] = new CellCandidate(i);
+
+let simples = 0;
+
+let candidates = 0;
+
+let setNaked2 = 0;
+let setNaked3 = 0;
+let setNaked4 = 0;
+let setHidden2 = 0;
+let setHidden3 = 0;
+let setHidden4 = 0;
+let omissionsReduced = 0;
+let yWingReduced = 0;
+let xyzWingReduced = 0;
+let xWingReduced = 0;
+let swordfishReduced = 0;
+let jellyfishReduced = 0;
+let uniqueRectangleReduced = 0;
+let phistomefelCount = 0;
+
+let superpositions = 0;
+let superpositionReduced = new Map();
+
+let bruteForceFill = 0;
+
+let maxTime = 0;
+let totalTime = 0;
+
+let puzzleString = null;
+let puzzleStrings = null;
+
+const clueCounter = new Map();
+
+let stepMode = 0; // 1=row 2=phist
+const step = () => {
+	const time = performance.now();
+
+	let mode = stepMode;
+	if (puzzleString) {
+		cells.fromString(puzzleString);
+		mode = -1;
+	}
+
+	let id = 0;
+	if (puzzleStrings) {
+		const puzzleData = puzzleStrings.shift();
+		if (!puzzleData) return false;
+		const puzzle = puzzleData.puzzleClues;
+		if (!puzzle) return false;
+		id = puzzleData.id;
+		cells.fromString(puzzle);
+		mode = -1;
+	}
+	const clueCount = sudokuGenerator(cells, mode);
+
+	const clueValue = clueCounter.get(clueCount);
+	if (clueValue) {
+		clueCounter.set(clueCount, clueValue + 1)
+	} else {
+		clueCounter.set(clueCount, 1)
+	}
+
+	const data = {
+		id,
+		puzzle: cells.string(),
+		totalPuzzles: totalPuzzles,
+		cells: cells.toData(),
+		message: null
+	};
+
+	for (const cell of cells) if (cell.symbol === 0) cell.fill();
+	const save = cells.toData();
+
+	const result = fillSolve(cells, STRATEGY.ALL);
+
+	data.puzzleClues = data.puzzle;
+	data.puzzleFilled = cells.string();
+	data.clueCount = clueCount;
+
+	data.simple = 0;
+	data.naked2 = 0;
+	data.naked3 = 0;
+	data.naked4 = 0;
+	data.hidden2 = 0;
+	data.hidden3 = 0;
+	data.hidden4 = 0;
+	data.omissions = 0;
+	data.yWing = 0;
+	data.xyzWing = 0;
+	data.xWing = 0;
+	data.swordfish = 0;
+	data.jellyfish = 0;
+	data.uniqueRectangle = 0;
+	data.phistomefel = 0;
+	data.superpositions = 0;
+	data.bruteForce = 0;
+
+	const elapsed = performance.now() - time;
+	if (maxTime === 0) {
+		maxTime = elapsed;
+	} else {
+		if (elapsed > maxTime) {
+			maxTime = elapsed;
+		}
+	}
+
+	totalTime += elapsed;
+
+	if (result.bruteForceFill) {
+		data.bruteForce = 1;
+		bruteForceFill++;
+	}
+
+	const phistomefelResult = (result.phistomefelReduced > 0 || result.phistomefelFilled > 0);
+
+	let simple = true;
+	simple &&= result.naked2Reduced === 0;
+	simple &&= result.naked3Reduced === 0;
+	simple &&= result.naked4Reduced === 0;
+	simple &&= result.hidden2Reduced === 0;
+	simple &&= result.hidden3Reduced === 0;
+	simple &&= result.hidden4Reduced === 0;
+	simple &&= result.omissionsReduced === 0;
+	simple &&= result.yWingReduced === 0;
+	simple &&= result.xyzWingReduced === 0;
+	simple &&= result.xWingReduced === 0;
+	simple &&= result.swordfishReduced === 0;
+	simple &&= result.jellyfishReduced === 0;
+	simple &&= result.uniqueRectangleReduced === 0;
+	simple &&= result.superpositionReduced.length === 0;
+	simple &&= !phistomefelResult;
+	simple &&= !result.bruteForceFill;
+
+	data.simple = simple ? 1 : 0;
+
+	data.has_naked2 = 0;
+	data.has_naked3 = 0;
+	data.has_naked4 = 0;
+	data.has_hidden2 = 0;
+	data.has_hidden3 = 0;
+	data.has_hidden4 = 0;
+	data.has_omissions = 0;
+	data.has_uniqueRectangle = 0;
+	data.has_yWing = 0;
+	data.has_xyzWing = 0;
+	data.has_xWing = 0;
+	data.has_swordfish = 0;
+	data.has_jellyfish = 0;
+
+	if (simple) simples++;
+	else {
+		const processStrategy = (resultProperty, dataProperty, strategy) => {
+			if (result[resultProperty] === 0) return;
+
+			cells.fromData(save);
+			const strategyResult = fillSolve(cells, strategy, false);
+			const strategyResultValue = strategyResult[resultProperty];
+
+			result[resultProperty] = strategyResultValue;
+			if (strategyResultValue > 0) {
+				cells.fromData(save);
+				const resultIsolated = fillSolve(cells, strategy, true);
+
+				if (!resultIsolated.bruteForceFill) {
+					const isolatedValue = resultIsolated[resultProperty];
+					if (isolatedValue <= strategyResultValue) data[dataProperty] = isolatedValue;
+				}
+			}
+		}
+		processStrategy('naked2Reduced', 'has_naked2', STRATEGY.NAKED_2);
+		processStrategy('naked3Reduced', 'has_naked3', STRATEGY.NAKED_3);
+		processStrategy('naked4Reduced', 'has_naked4', STRATEGY.NAKED_4);
+		processStrategy('hidden2Reduced', 'has_hidden2', STRATEGY.HIDDEN_2);
+		processStrategy('hidden3Reduced', 'has_hidden3', STRATEGY.HIDDEN_3);
+		processStrategy('hidden4Reduced', 'has_hidden4', STRATEGY.HIDDEN_4);
+		processStrategy('omissionsReduced', 'has_omissions', STRATEGY.INTERSECTION_REMOVAL);
+		processStrategy('uniqueRectangleReduced', 'has_uniqueRectangle', STRATEGY.DEADLY_PATTERN);
+		processStrategy('yWingReduced', 'has_yWing', STRATEGY.Y_WING);
+		processStrategy('xyzWingReduced', 'has_xyzWing', STRATEGY.XYZ_WING);
+		processStrategy('xWingReduced', 'has_xWing', STRATEGY.X_WING);
+		processStrategy('swordfishReduced', 'has_swordfish', STRATEGY.SWORDFISH);
+		processStrategy('jellyfishReduced', 'has_jellyfish', STRATEGY.JELLYFISH);
+
+		setNaked2 += data.has_naked2;
+		setNaked3 += data.has_naked3;
+		setNaked4 += data.has_naked4;
+		setHidden2 += data.has_hidden2;
+		setHidden3 += data.has_hidden3;
+		setHidden4 += data.has_hidden4;
+
+		data.naked2 += result.naked2Reduced;
+		data.naked3 += result.naked3Reduced;
+		data.naked4 += result.naked4Reduced;
+		data.hidden2 += result.hidden2Reduced;
+		data.hidden3 += result.hidden3Reduced;
+		data.hidden4 += result.hidden4Reduced;
+
+		omissionsReduced += data.has_omissions;
+		data.omissions += result.omissionsReduced;
+
+		yWingReduced += data.has_yWing;
+		data.yWing += result.yWingReduced;
+
+		xyzWingReduced += data.has_xyzWing;
+		data.xyzWing += result.xyzWingReduced;
+
+		xWingReduced += data.has_xWing;
+		data.xWing += result.xWingReduced;
+
+		swordfishReduced += data.has_swordfish;
+		data.swordfish += result.swordfishReduced;
+
+		jellyfishReduced += data.has_jellyfish;
+		data.jellyfish += result.jellyfishReduced;
+
+		uniqueRectangleReduced += data.has_uniqueRectangle;
+		data.uniqueRectangle += result.uniqueRectangleReduced;
+
+		if (phistomefelResult) phistomefelCount++;
+		if (phistomefelResult) data.phistomefel++;
+
+		if (result.superpositionReduced.length > 0) {
+			const once = new Set();
+			for (const superpositionResult of result.superpositionReduced) {
+				const key = superpositionResult.type + " " + superpositionResult.size;
+				if (once.has(key)) continue;
+
+				once.add(key);
+
+				const count = superpositionReduced.get(key);
+				if (count) {
+					superpositionReduced.set(key, count + 1);
+				} else {
+					superpositionReduced.set(key, 1);
+				}
+			}
+			superpositions++;
+			data.superpositions++;
+		}
+
+		if (!result.bruteForceFill) candidates++;
+	}
+
+	const res = 10000;
+	const percent = (val, total = totalPuzzles) => {
+		return Math.ceil(100 * res * val / total) / res + "%";
+	}
+
+	let candidateTotal = 0;
+	candidateTotal += setNaked2;
+	candidateTotal += setNaked3;
+	candidateTotal += setNaked4;
+	candidateTotal += setHidden2;
+	candidateTotal += setHidden3;
+	candidateTotal += setHidden4;
+	candidateTotal += omissionsReduced;
+	candidateTotal += yWingReduced;
+	candidateTotal += xyzWingReduced;
+	candidateTotal += xWingReduced;
+	candidateTotal += swordfishReduced;
+	candidateTotal += jellyfishReduced;
+	candidateTotal += uniqueRectangleReduced;
+	candidateTotal += phistomefelCount;
+
+	let superTotal = 0;
+	for (const value of superpositionReduced.values()) {
+		superTotal += value;
+	}
+
+	const printLine = (title, val, total) => {
+		lines.push(title + ": " + percent(val, total) + " - " + val);
+	};
+
+	const lines = [];
+
+	const clues = [...clueCounter.entries()];
+	clues.sort((a, b) => {
+		return a[0] - b[0];
+	});
+
+	lines.push("--- Clues");
+	for (const clue of clues) {
+		printLine(clue[0], clue[1], totalPuzzles);
+	}
+
+	if (superTotal > 0) {
+		lines.push("--- Superpositions");
+		const ordered = [];
+		const entries = superpositionReduced.entries();
+		for (const [key, value] of entries) {
+			ordered.push({ key, value });
+		}
+		ordered.sort((a, b) => {
+			return b.value - a.value;
+		});
+		for (const result of ordered) {
+			printLine(result.key, result.value, superTotal);
+		}
+	}
+	if (candidateTotal > 0) {
+		lines.push("--- Candidates");
+		printLine("Naked2", setNaked2, candidateTotal);
+		printLine("Naked3", setNaked3, candidateTotal);
+		printLine("Naked4", setNaked4, candidateTotal);
+		printLine("Hidden2", setHidden2, candidateTotal);
+		printLine("Hidden3", setHidden3, candidateTotal);
+		printLine("Hidden4", setHidden4, candidateTotal);
+
+		printLine("Omissions", omissionsReduced, candidateTotal);
+		printLine("UniqueRectangle", uniqueRectangleReduced, candidateTotal);
+		printLine("yWing", yWingReduced, candidateTotal);
+		printLine("xyzWing", xyzWingReduced, candidateTotal);
+		printLine("xWing", xWingReduced, candidateTotal);
+		printLine("Swordfish", swordfishReduced, candidateTotal);
+		printLine("Jellyfish", jellyfishReduced, candidateTotal);
+		if (mode === 2) printLine("Phistomefel", phistomefelCount, candidateTotal);
+	}
+
+	lines.push("--- Totals");
+	lines.push("Simples: " + percent(simples) + " - " + simples);
+	lines.push("Candidates: " + percent(candidates) + " - " + candidates);
+	// lines.push("Superpositions: " + percent(superpositions) + " - " + superpositions);
+	lines.push("BruteForceFill: " + percent(bruteForceFill) + " - " + bruteForceFill);
+	lines.push("Time Avg: " + totalTime / 1000 / totalPuzzles + " Max: " + maxTime / 1000);
+	lines.push("Puzzles: " + totalPuzzles);
+
+	data.message = lines;
+
+	postMessage(data);
+
+	return true;
+};
+
+onmessage = (e) => {
+	puzzleString = e.data.grid ?? null;
+	stepMode = 0; // (searchParams.get("table") == "phistomefel") ? 2 : 0;
+	if (e.data.grids) {
+		if (!puzzleStrings) puzzleStrings = [];
+		for (const data of e.data.grids) {
+			puzzleStrings.push(data);
+		}
+	}
+	while (step());
+};
