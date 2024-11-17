a404de9c61b913d656b33c01ca936116193fc8b2
ScottNovakowski <snovakow@gmail.com>
2024-11-17 18:07:07 -0500

    Update feedSudoku.php

--- feedSudoku.php
+++ feedSudoku.php
@@ -18,7 +18,14 @@ try {
 	$conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
 	// $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION, PDO::ATTR_STRINGIFY_FETCHES);
 
-	$stmt = $conn->prepare("SELECT * FROM `" . $table . "` WHERE `id`>" . $start . " AND `id`<=" . $end);
+	$stmt = $conn->prepare("SELECT 
+		`id`, HEX(`puzzleData`) AS 'puzzleData', `clueCount`, `simple`, `bruteForce`, 
+		`solveType`, `naked2`, `naked3`, `naked4`, `hidden2`, `hidden3`, `hidden4`, 
+		`omissions`, `uniqueRectangle`, `yWing`, `xyzWing`, `xWing`, `swordfish`, `jellyfish`, 
+		`has_naked2`, `has_naked3`, `has_naked4`, `has_hidden2`, `has_hidden3`, `has_hidden4`, 
+		`has_omissions`, `has_uniqueRectangle`, `has_yWing`, `has_xyzWing`, 
+		`has_xWing`, `has_swordfish`, `has_jellyfish` 
+		FROM `" . $table . "` WHERE `id`>" . $start . " AND `id`<=" . $end);
 	$stmt->execute();
 
 	$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
