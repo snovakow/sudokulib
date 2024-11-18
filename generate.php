8f0cf7d9d2354f8527ec9e809c1ecc3d51a8c2b2
ScottNovakowski <snovakow@gmail.com>
2024-11-18 13:08:49 -0500

    Update generate.php

--- generate.php
+++ generate.php
@@ -1,83 +1,17 @@
 <?php
-if (!isset($_GET['version'])) die();
 $version = 1;
+if (!isset($_GET['version'])) die();
 if ($_GET['version'] != $version) die();
-
-if (!isset($_GET['puzzleData'])) die();
-if (!isset($_GET['clueCount'])) die();
-if (!isset($_GET['simple'])) die();
-if (!isset($_GET['naked2'])) die();
-if (!isset($_GET['naked3'])) die();
-if (!isset($_GET['naked4'])) die();
-if (!isset($_GET['hidden2'])) die();
-if (!isset($_GET['hidden3'])) die();
-if (!isset($_GET['hidden4'])) die();
-if (!isset($_GET['omissions'])) die();
-if (!isset($_GET['yWing'])) die();
-if (!isset($_GET['xyzWing'])) die();
-if (!isset($_GET['xWing'])) die();
-if (!isset($_GET['swordfish'])) die();
-if (!isset($_GET['jellyfish'])) die();
-if (!isset($_GET['uniqueRectangle'])) die();
-if (!isset($_GET['bruteForce'])) die();
-
-if (!isset($_GET['has_naked2'])) die();
-if (!isset($_GET['has_naked3'])) die();
-if (!isset($_GET['has_naked4'])) die();
-if (!isset($_GET['has_hidden2'])) die();
-if (!isset($_GET['has_hidden3'])) die();
-if (!isset($_GET['has_hidden4'])) die();
-if (!isset($_GET['has_omissions'])) die();
-if (!isset($_GET['has_uniqueRectangle'])) die();
-if (!isset($_GET['has_yWing'])) die();
-if (!isset($_GET['has_xyzWing'])) die();
-if (!isset($_GET['has_xWing'])) die();
-if (!isset($_GET['has_swordfish'])) die();
-if (!isset($_GET['has_jellyfish'])) die();
 
 if (!isset($_GET['table'])) die();
 $table = $_GET['table'];
-
-$puzzleData = $_GET['puzzleData'];
-$clueCount = $_GET['clueCount'];
-$simple = $_GET['simple'];
-$naked2 = $_GET['naked2'];
-$naked3 = $_GET['naked3'];
-$naked4 = $_GET['naked4'];
-$hidden2 = $_GET['hidden2'];
-$hidden3 = $_GET['hidden3'];
-$hidden4 = $_GET['hidden4'];
-$omissions = $_GET['omissions'];
-$yWing = $_GET['yWing'];
-$xyzWing = $_GET['xyzWing'];
-$xWing = $_GET['xWing'];
-$swordfish = $_GET['swordfish'];
-$jellyfish = $_GET['jellyfish'];
-$uniqueRectangle = $_GET['uniqueRectangle'];
-$bruteForce = $_GET['bruteForce'];
 
 $servername = "localhost";
 $username = "snovakow";
 $password = "kewbac-recge1-Fiwpux";
 $dbname = "sudoku";
 
-$solveType = 1;
-if ($simple > 0) $solveType = 0;
-if ($bruteForce > 0) $solveType = 2;
-
-$has_naked2 = $_GET['has_naked2'];
-$has_naked3 = $_GET['has_naked3'];
-$has_naked4 = $_GET['has_naked4'];
-$has_hidden2 = $_GET['has_hidden2'];
-$has_hidden3 = $_GET['has_hidden3'];
-$has_hidden4 = $_GET['has_hidden4'];
-$has_omissions = $_GET['has_omissions'];
-$has_uniqueRectangle = $_GET['has_uniqueRectangle'];
-$has_yWing = $_GET['has_yWing'];
-$has_xyzWing = $_GET['has_xyzWing'];
-$has_xWing = $_GET['has_xWing'];
-$has_swordfish = $_GET['has_swordfish'];
-$has_jellyfish = $_GET['has_jellyfish'];
+$array = json_decode(file_get_contents("php://input"));
 
 try {
 	$pdo = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
@@ -96,64 +30,136 @@ try {
 			SELECT table_name FROM information_schema.tables WHERE table_schema = 'sudoku' AND table_name = '" . $table . "' LIMIT 1;
 		");
 		$stmt->execute();
-		if ($stmt->fetch()["table_name"] === NULL) exit("-1");
+		if ($stmt->fetch()["table_name"] === NULL) exit("-1:0");
 		$count = 0;
 	}
 
-	if ($count >= 10000000) {
-		exit("0");
-	}
+	$maxSize = 10000000;
+	if ($count >= $maxSize) exit("0:0");
 
 	$sql = "INSERT INTO `" . $table . "` (puzzleData, clueCount, 
 			simple, naked2, naked3, naked4, hidden2, hidden3, hidden4, omissions, 
 			yWing, xyzWing, xWing, swordfish, jellyfish, uniqueRectangle, 
 			has_naked2, has_naked3, has_naked4, has_hidden2, has_hidden3, has_hidden4, has_omissions, 
 			has_yWing, has_xyzWing, has_xWing, has_swordfish, has_jellyfish, has_uniqueRectangle, 
-			bruteForce, solveType) 
-			VALUES (X'" . $puzzleData . "', :clueCount, :simple, 
-			:naked2, :naked3, :naked4, :hidden2, :hidden3, :hidden4, :omissions, 
-			:yWing, :xyzWing, :xWing, :swordfish, :jellyfish, :uniqueRectangle, 
-			:has_naked2, :has_naked3, :has_naked4, :has_hidden2, :has_hidden3, :has_hidden4, :has_omissions, 
-			:has_yWing, :has_xyzWing, :has_xWing, :has_swordfish, :has_jellyfish, :has_uniqueRectangle, 
-			:bruteForce, :solveType)";
-	$stmt = $pdo->prepare($sql);
+			bruteForce, solveType) VALUES ";
 
-	$stmt->bindValue('clueCount', $clueCount, PDO::PARAM_INT);
-	$stmt->bindValue('simple', $simple, PDO::PARAM_INT);
-	$stmt->bindValue('naked2', $naked2, PDO::PARAM_INT);
-	$stmt->bindValue('naked3', $naked3, PDO::PARAM_INT);
-	$stmt->bindValue('naked4', $naked4, PDO::PARAM_INT);
-	$stmt->bindValue('hidden2', $hidden2, PDO::PARAM_INT);
-	$stmt->bindValue('hidden3', $hidden3, PDO::PARAM_INT);
-	$stmt->bindValue('hidden4', $hidden4, PDO::PARAM_INT);
-	$stmt->bindValue('omissions', $omissions, PDO::PARAM_INT);
-	$stmt->bindValue('yWing', $yWing, PDO::PARAM_INT);
-	$stmt->bindValue('xyzWing', $xyzWing, PDO::PARAM_INT);
-	$stmt->bindValue('xWing', $xWing, PDO::PARAM_INT);
-	$stmt->bindValue('swordfish', $swordfish, PDO::PARAM_INT);
-	$stmt->bindValue('jellyfish', $jellyfish, PDO::PARAM_INT);
-	$stmt->bindValue('uniqueRectangle', $uniqueRectangle, PDO::PARAM_INT);
-	$stmt->bindValue('has_naked2', $has_naked2, PDO::PARAM_INT);
-	$stmt->bindValue('has_naked3', $has_naked3, PDO::PARAM_INT);
-	$stmt->bindValue('has_naked4', $has_naked4, PDO::PARAM_INT);
-	$stmt->bindValue('has_hidden2', $has_hidden2, PDO::PARAM_INT);
-	$stmt->bindValue('has_hidden3', $has_hidden3, PDO::PARAM_INT);
-	$stmt->bindValue('has_hidden4', $has_hidden4, PDO::PARAM_INT);
-	$stmt->bindValue('has_omissions', $has_omissions, PDO::PARAM_INT);
-	$stmt->bindValue('has_yWing', $has_yWing, PDO::PARAM_INT);
-	$stmt->bindValue('has_xyzWing', $has_xyzWing, PDO::PARAM_INT);
-	$stmt->bindValue('has_xWing', $has_xWing, PDO::PARAM_INT);
-	$stmt->bindValue('has_swordfish', $has_swordfish, PDO::PARAM_INT);
-	$stmt->bindValue('has_jellyfish', $has_jellyfish, PDO::PARAM_INT);
-	$stmt->bindValue('has_uniqueRectangle', $has_uniqueRectangle, PDO::PARAM_INT);
-	$stmt->bindValue('bruteForce', $bruteForce, PDO::PARAM_INT);
-	$stmt->bindValue('solveType', $solveType, PDO::PARAM_INT);
+	$inserted = 0;
+	$values = array();
+	foreach ($array as $post) {
+		if (!isset($post->puzzleData)) continue;
+		if (!isset($post->clueCount)) continue;
+		if (!isset($post->simple)) continue;
+		if (!isset($post->naked2)) continue;
+		if (!isset($post->naked3)) continue;
+		if (!isset($post->naked4)) continue;
+		if (!isset($post->hidden2)) continue;
+		if (!isset($post->hidden3)) continue;
+		if (!isset($post->hidden4)) continue;
+		if (!isset($post->omissions)) continue;
+		if (!isset($post->yWing)) continue;
+		if (!isset($post->xyzWing)) continue;
+		if (!isset($post->xWing)) continue;
+		if (!isset($post->swordfish)) continue;
+		if (!isset($post->jellyfish)) continue;
+		if (!isset($post->uniqueRectangle)) continue;
+		if (!isset($post->bruteForce)) continue;
 
-	$stmt->execute();
+		if (!isset($post->has_naked2)) continue;
+		if (!isset($post->has_naked3)) continue;
+		if (!isset($post->has_naked4)) continue;
+		if (!isset($post->has_hidden2)) continue;
+		if (!isset($post->has_hidden3)) continue;
+		if (!isset($post->has_hidden4)) continue;
+		if (!isset($post->has_omissions)) continue;
+		if (!isset($post->has_uniqueRectangle)) continue;
+		if (!isset($post->has_yWing)) continue;
+		if (!isset($post->has_xyzWing)) continue;
+		if (!isset($post->has_xWing)) continue;
+		if (!isset($post->has_swordfish)) continue;
+		if (!isset($post->has_jellyfish)) continue;
+
+		$puzzleData = $post->puzzleData;
+		$clueCount = $post->clueCount;
+		$simple = $post->simple;
+		$naked2 = $post->naked2;
+		$naked3 = $post->naked3;
+		$naked4 = $post->naked4;
+		$hidden2 = $post->hidden2;
+		$hidden3 = $post->hidden3;
+		$hidden4 = $post->hidden4;
+		$omissions = $post->omissions;
+		$yWing = $post->yWing;
+		$xyzWing = $post->xyzWing;
+		$xWing = $post->xWing;
+		$swordfish = $post->swordfish;
+		$jellyfish = $post->jellyfish;
+		$uniqueRectangle = $post->uniqueRectangle;
+		$bruteForce = $post->bruteForce;
+
+		$solveType = 1;
+		if ($simple > 0) $solveType = 0;
+		if ($bruteForce > 0) $solveType = 2;
+
+		$has_naked2 = $post->has_naked2;
+		$has_naked3 = $post->has_naked3;
+		$has_naked4 = $post->has_naked4;
+		$has_hidden2 = $post->has_hidden2;
+		$has_hidden3 = $post->has_hidden3;
+		$has_hidden4 = $post->has_hidden4;
+		$has_omissions = $post->has_omissions;
+		$has_uniqueRectangle = $post->has_uniqueRectangle;
+		$has_yWing = $post->has_yWing;
+		$has_xyzWing = $post->has_xyzWing;
+		$has_xWing = $post->has_xWing;
+		$has_swordfish = $post->has_swordfish;
+		$has_jellyfish = $post->has_jellyfish;
+
+		$valueList = array(
+			$clueCount,
+			$simple,
+			$naked2,
+			$naked3,
+			$naked4,
+			$hidden2,
+			$hidden3,
+			$hidden4,
+			$omissions,
+			$yWing,
+			$xyzWing,
+			$xWing,
+			$swordfish,
+			$jellyfish,
+			$uniqueRectangle,
+			$has_naked2,
+			$has_naked3,
+			$has_naked4,
+			$has_hidden2,
+			$has_hidden3,
+			$has_hidden4,
+			$has_omissions,
+			$has_yWing,
+			$has_xyzWing,
+			$has_xWing,
+			$has_swordfish,
+			$has_jellyfish,
+			$has_uniqueRectangle,
+			$bruteForce,
+			$solveType
+		);
+		$values[] = "(X'" . $puzzleData . "'," . implode(",", $valueList) . ")";
+
+		$count++;
+		$inserted++;
+		if ($count >= $maxSize) break;
+	}
+
+	$sql .= implode(",", $values);
+	$pdo->exec($sql);
 
 	$pdo->exec('UNLOCK TABLES');
 
-	echo ($count + 1);
+	echo ($count . ":" . $inserted);
 } catch (PDOException $e) {
 	// echo "Connection failed: " . $e->getMessage();
 }
