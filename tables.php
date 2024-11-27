27409eb06d94e289fd55d37604b8ab19a3b2251d
ScottNovakowski <snovakow@gmail.com>
2024-11-27 12:26:31 -0500

    Create tables.php

---
+++ tables.php
@@ -0,0 +1,20 @@
+<?php
+if (!isset($_GET['version'])) die();
+$version = (int)$_GET['version'];
+if ($version !== 1) die();
+
+try {
+	$servername = "localhost";
+	$username = "snovakow";
+	$password = "kewbac-recge1-Fiwpux";
+	$dbname = "sudoku";
+	$db = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+
+	$stmt = $db->prepare("SELECT `tableCount`, `puzzleCount` FROM `tables`");
+	$stmt->execute();
+	$result = $stmt->fetch();
+
+	echo $result["tableCount"] . ":" . $result["puzzleCount"];
+} catch (PDOException $e) {
+	// echo "Error: " . $e->getMessage();
+}
