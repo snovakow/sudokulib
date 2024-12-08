e589f1f435fc39ea2c16209e8f32c822a92e51de
ScottNovakowski <snovakow@gmail.com>
2024-12-08 11:49:35 -0500

    Update feedSudoku.php

--- feedSudoku.php
+++ feedSudoku.php
@@ -1,6 +1,4 @@
 <?php
-
-// header("Access-Control-Allow-Origin: *");
 if (!isset($_GET['start'])) die;
 if (!isset($_GET['end'])) die;
 if (!isset($_GET['table'])) die;
@@ -9,28 +7,18 @@ $start = (int)$_GET['start'];
 $end = (int)$_GET['end'];
 $table = $_GET['table'];
 
-$servername = "localhost";
-$username = "snovakow";
-$password = "kewbac-recge1-Fiwpux";
-$dbname = "sudoku";
-
 try {
-	$conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-	// $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION, PDO::ATTR_STRINGIFY_FETCHES);
+	$servername = "localhost";
+	$username = "snovakow";
+	$password = "kewbac-recge1-Fiwpux";
+	$dbname = "sudoku";
+	$db = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
 
-	$stmt = $conn->prepare("SELECT 
-		`id`, HEX(`puzzleData`) AS 'puzzleData', `clueCount`, `simple`, `bruteForce`, 
-		`solveType`, `naked2`, `naked3`, `naked4`, `hidden2`, `hidden3`, `hidden4`, 
-		`omissions`, `uniqueRectangle`, `yWing`, `xyzWing`, `xWing`, `swordfish`, `jellyfish`, 
-		`has_naked2`, `has_naked3`, `has_naked4`, `has_hidden2`, `has_hidden3`, `has_hidden4`, 
-		`has_omissions`, `has_uniqueRectangle`, `has_yWing`, `has_xyzWing`, 
-		`has_xWing`, `has_swordfish`, `has_jellyfish` 
-		FROM `" . $table . "` WHERE `id`>" . $start . " AND `id`<=" . $end);
+	$stmt = $db->prepare("SELECT `id`, HEX(`puzzleData`) AS 'puzzleData' FROM `$table` WHERE `id`>$start AND `id`<=$end");
 	$stmt->execute();
 
 	$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
 	echo json_encode($result);
 } catch (PDOException $e) {
-	echo "Error: " . $e->getMessage();
+	// echo "Error: " . $e->getMessage();
 }
-$conn = null;
